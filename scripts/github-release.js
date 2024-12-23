233f0c744c8c06bc4f35cb80ce3ebbd452450dd9
Wei-Chi Huang <tidenundbegonia@hotmail.com>
2024-12-23 22:01:09 +0100

    Initial commit

---
+++ scripts/github-release.js
@@ -0,0 +1,60 @@
+function timeSince(date) {
+    // https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = seconds;
+    var timeUnit = Math.floor(interval);
+    const dividers = [31536000, 2592000, 86400, 3600, 60, 1];
+    const names = ["year", "month", "day", "hour", "minute", "second"];
+
+    // loop over time units
+    timeText = timeUnit + " seconds"; // default to seconds
+    for (let counter = 0; counter < dividers.length; counter++) {
+        interval = seconds / dividers[counter];
+        timeUnit = Math.floor(interval);
+        if (interval >= 1) {
+            if (timeUnit == 1) {
+                timeText =  timeUnit + " " + names[counter];
+            } else {
+                timeText = timeUnit + " " + names[counter] + "s";
+            }
+            break
+        }
+    }
+
+    // check if confetti (< 1 day)
+    doConfetti = seconds <= 24 * 60 * 60;
+
+    return [timeText,doConfetti]
+}
+
+
+getJSON('https://api.github.com/repos/TUMcps/AROC/commits?per_page=1',
+    function(err, data) {
+        if (err !== null) {
+            alert('Something went wrong: ' + err);
+        } else {
+            commit = data[0].commit;
+            message = commit.message;
+            date = commit.author.date;
+            [timeText, doConfetti] = timeSince(Date.parse(date));
+
+            container = document.getElementById("release-version");
+            container.innerHTML = `<object class="align-middle" data="https://img.shields.io/static/v1?label=Last update&message=${timeText} ago&color=4596FF" alt="TUMcps - AROC"></object>`;
+
+            // confetti
+            if (doConfetti) {
+                const delay = ms => new Promise(res => setTimeout(res, ms));
+                const yourFunction = async () => {
+                    await delay(1000);
+                    confetti({
+                        particleCount: 100,
+                        spread: 70,
+                        origin: { y: 0.3 },
+                    });
+                };
+                yourFunction();
+            }
+        }
+    });
